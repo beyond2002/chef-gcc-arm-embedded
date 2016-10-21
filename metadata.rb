a106121bc80822a91bd371c8fb15f6cd4a5def35
tenacious <fbeachler@gmail.com>
2016-10-21 00:45:34 -0600

    bump minor-minor ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'longevitysoft@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures gcc-arm-embedded'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 depends          'build-essential'
 depends          'apt'
 depends          'ark'
