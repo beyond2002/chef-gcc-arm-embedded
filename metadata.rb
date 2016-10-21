60a1a5d0e810d53e4a57fe5c940a5a10f8b3a360
tenacious <fbeachler@gmail.com>
2016-10-20 23:33:55 -0600

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'longevitysoft@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures gcc-arm-embedded'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 depends          'build-essential'
 depends          'apt'
 depends          'ark'
