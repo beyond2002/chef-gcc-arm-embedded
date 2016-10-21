648ce3ab7705612d1e3c7f4a86fc13374a2e02df
tenacious <fbeachler@gmail.com>
2016-10-21 00:19:53 -0600

    update metadata license to comply with readme

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'gcc-arm-embedded'
 maintainer       'Free Beachler'
 maintainer_email 'longevitysoft@gmail.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures gcc-arm-embedded'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
